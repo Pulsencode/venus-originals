827456ae20eba6933980558171ddcf6348aea041
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2025-05-07 18:43:04 +0530

    converted to php

---
+++ includes/components/our_expertise.php
@@ -0,0 +1,54 @@
+<section>
+    <div class="container">
+        <div class="cs-funfact_wrap cs-type1">
+            <div class="cs-funfact_shape" data-src="assets/img/funfact_shape_bg.svg"></div>
+            <div class="cs-funfact_left">
+                <div class="cs-funfact_heading">
+                    <h2>Our Expertise</h2>
+                    <p> Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem
+                        aperiam, eaque ipsa quae ab illo inventore veritatis. </p>
+                </div>
+            </div>
+            <div class="cs-funfact_right">
+                <div class="cs-funfacts">
+                    <div class="cs-funfact cs-style1">
+                        <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
+                            <span data-count-to="40" class="odometer"></span>K
+                        </div>
+                        <div class="cs-funfact_text">
+                            <span class="cs-accent_color">+</span>
+                            <p>Motion Graphics</p>
+                        </div>
+                    </div>
+                    <div class="cs-funfact cs-style1">
+                        <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
+                            <span data-count-to="10" class="odometer"></span>
+                        </div>
+                        <div class="cs-funfact_text">
+                            <span class="cs-accent_color">+</span>
+                            <p>3D Animation</p>
+                        </div>
+                    </div>
+                    <div class="cs-funfact cs-style1">
+                        <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
+                            <span data-count-to="245" class="odometer"></span>
+                        </div>
+                        <div class="cs-funfact_text">
+                            <span class="cs-accent_color">+</span>
+                            <p>CGI & VFX</p>
+                        </div>
+                    </div>
+                    <div class="cs-funfact cs-style1">
+                        <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
+                            <span data-count-to="550" class="odometer"></span>
+                        </div>
+                        <div class="cs-funfact_text">
+                            <span class="cs-accent_color">+</span>
+                            <p>2D Animation</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
